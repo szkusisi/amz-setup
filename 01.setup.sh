1806255f32c02f1998a3ac6f0d99a750596f3b10
szkusisi <szk@usisi.net>
2020-11-20 07:14:36 +0000

    Default Message

--- 01.setup.sh
+++ 01.setup.sh
@@ -3,6 +3,6 @@ sudo yum upgrade -y
 sudo amazon-linux-extras install epel -y
 sudo yum install -y git
 
-
-#sudo yum install -y certbot python3-certbot-apache httpd mod_ssl
+#
+# sudo yum install -y certbot python3-certbot-apache httpd mod_ssl
 
