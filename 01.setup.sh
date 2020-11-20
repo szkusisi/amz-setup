e3293aad4f6b4e2df81927952037c6afb44d7fa1
szkusisi <szk@usisi.net>
2020-11-20 07:16:02 +0000

    Default Message

--- 01.setup.sh
+++ 01.setup.sh
@@ -3,6 +3,14 @@ sudo yum upgrade -y
 sudo amazon-linux-extras install epel -y
 sudo yum install -y git
 
-#
+# git
+git config --global user.name "szkusisi"
+git config --global user.email "szk@usisi.net"
+git config --global credential.helper store
+
+# gitauto
+
+
+
 # sudo yum install -y certbot python3-certbot-apache httpd mod_ssl
 
