6c5ccc4c48674e9578253766513cf81862ef4a92
szkusisi <szk@usisi.net>
2020-11-21 13:51:56 +0900

    Default Message

--- 01.setup.sh
+++ 01.setup.sh
@@ -6,6 +6,9 @@ git config --global user.name "szkusisi"
 git config --global user.email "szk@usisi.net"
 git config --global credential.helper store
 
+# timezone
+sudo timedatectl set-timezone Asia/Tokyo
+
 # gitauto コピー
 sudo cp bin/gitauto /usr/local/bin/
 /usr/local/bin/gitauto
