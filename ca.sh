a778d7b3e5559cffc82dc7624f52de91f1a3538c
szkusisi <51690418+szkusisi@users.noreply.github.com>
2020-11-24 17:21:11 +0900

    Update ca.sh

--- ca.sh
+++ ca.sh
@@ -1,2 +1,3 @@
 https://rohhie.net/certification-authority-to-make-at-company/
 https://dev.classmethod.jp/articles/manage-aws-client-vpn-client-certificates/
+https://docs.aws.amazon.com/ja_jp/vpn/latest/clientvpn-admin/client-authentication.html#mutual
