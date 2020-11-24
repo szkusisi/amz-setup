4c76828643a9ddde9a3e5e55567e06931c31fad0
szkusisi <51690418+szkusisi@users.noreply.github.com>
2020-11-24 21:35:54 +0900

    Update ca.sh

--- ca.sh
+++ ca.sh
@@ -1,7 +1,7 @@
 https://rohhie.net/certification-authority-to-make-at-company/
 https://dev.classmethod.jp/articles/manage-aws-client-vpn-client-certificates/
 https://docs.aws.amazon.com/ja_jp/vpn/latest/clientvpn-admin/client-authentication.html#mutual
-
+https://server-setting.info/centos/openvpn-revoke-client.html#revoke
 
 
 git clone https://github.com/OpenVPN/easy-rsa.git
