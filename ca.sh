96c380430ca57841679d82cac82f4816cfc93edb
szkusisi <51690418+szkusisi@users.noreply.github.com>
2020-11-26 16:47:41 +0900

    Update ca.sh

--- ca.sh
+++ ca.sh
@@ -4,7 +4,8 @@ https://docs.aws.amazon.com/ja_jp/vpn/latest/clientvpn-admin/client-authenticati
 https://server-setting.info/centos/openvpn-revoke-client.html#revoke
 https://qiita.com/worldmotor/items/f81718ada1433ba9c71c
 
-
+https://dotnetdevelopmentinfrastructure.osscons.jp/index.php?%E3%82%AF%E3%83%A9%E3%82%A4%E3%82%A2%E3%83%B3%E3%83%88%E8%A8%BC%E6%98%8E%E6%9B%B8%E3%81%AE%E8%AA%8D%E8%A8%BC%E3%82%92%E4%B8%AD%E9%96%93%E5%B1%A4%E3%81%AB%E3%82%AA%E3%83%95%E3%83%AD%E3%83%BC%E3%83%89%E3%81%99%E3%82%8B%E3%80%82
+https://nec-baas.github.io/baas-manual/latest/developer/ja/auth/clientcert/configurations.html
 
 https://server-setting.info/centos/private-ca-cert.html
 =============================================================
