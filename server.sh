f0f382e7986a402a043c9d58ab402e7c793092fc
szkusisi <51690418+szkusisi@users.noreply.github.com>
2020-12-05 23:53:56 +0900

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,3 +1,20 @@
 HOSTNAME=test123
 
+
+
 sudo hostnamectl set-hostname $HOSTNAME
+sudo timedatectl set-timezone Asia/Tokyo
+
+sudo yum install -y httpd mod_ssl
+
+sudo systemctl status httpd
+sudo systemctl start httpd
+sudo systemctl status httpd
+sudo systemctl enable httpd
+
+
+# htmlファイル作成
+cd /var/www/html/
+sudo tee index.html <<EOF
+$HOSTNAME
+EOF
