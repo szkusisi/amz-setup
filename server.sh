0ed08fd9abd9c6f474b2396cb3ad515e9bebffd2
szkusisi <51690418+szkusisi@users.noreply.github.com>
2020-12-05 23:55:49 +0900

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,11 +1,16 @@
 HOSTNAME=test123
 
-
-
 sudo hostnamectl set-hostname $HOSTNAME
 sudo timedatectl set-timezone Asia/Tokyo
 
+
 sudo yum install -y httpd mod_ssl
+# Apache ConfのServerName変更
+# エラーでるため
+
+# Virtualhost
+# SSL
+
 
 sudo systemctl status httpd
 sudo systemctl start httpd
